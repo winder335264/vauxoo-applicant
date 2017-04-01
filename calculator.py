82a1d0a144e5c3f824ab1f9b84ed696e4c3f619f
winder335264 <winder335264@gmail.com>
2017-04-01 18:25:54 +0000

    Add files via upload
    calculatorrrrr

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
 """
-Modulo suma
+Module with arithmetic operations
 """
 
 
 class CalculatorClass(object):
     """
-    Clase suma
+    Class with arithmetic operations
     """
 
     def sum(self, num_list):
         """
-        Metodo para sumar
+        Method that receives a list of numbers and adds them
         """
         total = 0
         for number in num_list:
             total += number
-        return total​
+        return total
