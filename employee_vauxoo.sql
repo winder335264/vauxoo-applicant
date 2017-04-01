bf4444df8cb9c4363f00e22f0537a45d413f8718
winder335264 <winder335264@gmail.com>
2017-04-01 16:07:10 +0000

    Add files via upload
    commit sql 1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,13 +3,44 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
-);
 
 CREATE TABLE employee_department (
+  id integer PRIMARY KEY,
+  name varchar (60),
+  description varchar (150)
 );
 
-CREATE TABLE employee_hobby (
+
+INSERT INTO employee_department (id, name, description) VALUES
+(1,'Produccion','Fabrica la produccion y mantiene el stock'),
+(2,'Compras','Adquiere materias prima'),
+(3,'Administracion','contabiliza las facturas emitidas y recibidas'),
+(4,'Financiero','consigue la financiacion para la empresa'),
+(5,'Maketing','Colabora con el comercial para conseguir mas ventas'),
+(6,'Recursos Humanos','gestiona a las personas para garantizar el cumplimiento de las funciones');
+
+CREATE TABLE employee (
+  id integer PRIMARY KEY,
+  first_name varchar (60),
+  last_name varchar (150),
+  -- cambio para poder asignarle a los empleados su departamento
+  id_department integer REFERENCES employee_department
 );
+
+INSERT INTO employee (id,first_name,last_name,id_department) VALUES
+(1,'Winder','Morillo',6),
+(2,'Jose','Fernandez',2),
+(3,'Rosa','Olmedo',4),
+(4,'Wilmer','Yepez',5);
+
+CREATE TABLE employee_hobby (
+  id integer PRIMARY KEY,
+  name varchar (60),
+  description varchar (150)
+);
+
+
+
+  
 
 -- ...
