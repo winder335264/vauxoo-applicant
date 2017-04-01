7fc9e9e054c8ec3f29945c27e0cef9e24c8f90e2
winder335264 <winder335264@gmail.com>
2017-04-01 16:30:35 +0000

    Add files via upload
    commit sql 3

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,35 +4,35 @@
 --       Consider add ';' at end sentence.
 
 
-CREATE TABLE employee_hobby (
+CREATE TABLE employee_department (
   id integer PRIMARY KEY,
   name varchar (60),
   description varchar (150)
 );
 
 
-INSERT INTO employee_hobby (id,name,description) VALUES
-(1,'bailar','estimula el sentido de la responsabiliadad'),
-(2,'Fotografia','tomar fotos'),
-(3,'correr','te ayuda a relajarte y mantener en forma');  
-  
+INSERT INTO employee_department (id, name, description) VALUES
+(1,'Produccion','Fabrica la produccion y mantiene el stock'),
+(2,'Compras','Adquiere materias prima'),
+(3,'Administracion','contabiliza las facturas emitidas y recibidas'),
+(4,'Financiero','consigue la financiacion para la empresa'),
+(5,'Maketing','Colabora con el comercial para conseguir mas ventas'),
+(6,'Recursos Humanos','gestiona a las personas para garantizar el cumplimiento de las funciones');
 
-CREATE TABLE employees_hobbies (
-employee_id integer REFERENCES employee (id),
-hobby_id integer REFERENCES employee_hobby (id),
-PRIMARY KEY (employee_id,hobby_id)
+CREATE TABLE employee (
+  id integer PRIMARY KEY,
+  first_name varchar (60),
+  last_name varchar (150),
+  -- cambio para poder asignarle a los empleados su departamento
+  id_department integer REFERENCES employee_department (id),
+  --cambio para poder asignar un jefe a los empleados
+  id_boss integer REFERENCES employee (id)
 );
 
-INSERT INTO employees_hobbies (employee_id,hobby_id) VALUES 
-(1,2),
-(1,3),
+INSERT INTO employee (id,first_name,last_name,id_department,id_boss) VALUES
+(1,'Winder','Morillo',6,3),
+(2,'Jose','Fernandez',2,1),
+(3,'Rosa','Olmedo',4,1),
+(4,'Wilmer','Yepez',5,1);
 
-(2,3),
-(2,1),
-
-(3,1),
-(3,2),
-
-(4,1),
-(4,3);
 -- ...
